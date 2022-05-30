724f5a835cba16be3647d229d2e60c8c6dee4262
joycodes <jnguttu@gmail.com>
2022-05-30 03:48:26 +0300

    psql

--- personal/settings.py
+++ personal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary
 import django_heroku
 import dj_database_url
 from decouple import config,Csv
